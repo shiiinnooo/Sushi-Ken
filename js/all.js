9b5a53bc423903cde66d26e0313f474ea226961b
Shino <smile0919a@gmail.com>
2022-01-05 16:19:46 +0800

    Feat 新增選單

--- js/all.js
+++ js/all.js
@@ -19,3 +19,29 @@ const menuSwiper = new Swiper('.menu-swiper', {
   loop: true,
   effect: 'fade',
 });
+
+const checkboxMenu = document.querySelector('.checkbox');
+const menuItemBlock = document.querySelector('.menu-items');
+const menuItems = document.querySelectorAll('.menu-items li');
+const html = document.querySelector('html');
+checkboxMenu.addEventListener('change', () => {
+  if (checkboxMenu.checked) {
+    html.style.height = '100vh';
+    html.style.overflowY = 'hidden';
+    menuItemBlock.style.transform = 'translateY(0)';
+  } else {
+    html.style.height = '600vh';
+    html.style.overflowY = 'visible';
+    menuItemBlock.style.transform = 'translateY(-100%)';
+  } 
+});
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    console.log(item, 'click');
+    const html = document.querySelector('html');
+    html.style.height = '600vh';
+    html.style.overflowY = 'visible';
+    menuItemBlock.style.transform = 'translateY(-100%)';
+    checkboxMenu.checked = false;
+  })
+})
